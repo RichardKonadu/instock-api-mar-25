516cb26e650dc3cc9db00cba122f41d47aecf1e7
Israel Akinoso <akinosoissy@gmail.com>
2025-03-18 17:17:14 +0000

    Configure routes and basic MVC structre

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import inventoryRoutes from "./routes/inventoryRoutes.js";
+import warehouseRoutes from "./routes/warehouseRoutes.js";
 
 const PORT = process.env.PORT || 5050;
 
@@ -8,6 +10,13 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({ origin: process.env.FRONTEND_URL }));
+
+app.get("/", (_req, res) => {
+  res.send("App is running...");
+});
+
+app.use("/warehouses", warehouseRoutes);
+app.use("/inventories", inventoryRoutes);
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
