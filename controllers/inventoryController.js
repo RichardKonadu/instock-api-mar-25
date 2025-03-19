7921770064e87244253ba9ccbd923a60066cb26f
Richard Oppong-Konadu <richardok@gmail.com>
2025-03-19 15:43:03 +0000

    add join for warehouse_name on getallinventories

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -1,13 +1,25 @@
 import connection from "../utils/mysql.js";
 
 const getAllInventories = async (_req, res) => {
-  const sql = "SELECT * from inventories";
+  const sql = `
+    SELECT
+        i.id,
+        w.warehouse_name,
+        i.item_name,
+        i.description,
+        i.category,
+        i.status,
+        i.quantity
+    FROM
+        inventories i
+    JOIN warehouses w ON i.warehouse_id = w.id;
+  `;
 
   try {
     const [results] = await connection.query(sql);
 
     if (!results.length) {
-      res.status(404).json({ msg: "Error fethcing Inventories" });
+      res.status(404).json({ msg: "Error fetching Inventories" });
     }
 
     res.json(results);
