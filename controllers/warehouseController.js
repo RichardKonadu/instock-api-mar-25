6d73b589ea328a33ae9c86cd22fd0d060351efb6
Robell <robellasfaw@gmail.com>
2025-03-23 12:26:30 +0000

    Update patch request

--- controllers/warehouseController.js
+++ controllers/warehouseController.js
@@ -62,7 +62,7 @@ const addWarehouse = async (req, res) => {
   const validationResult = validateWarehouseForm(formData);
 
   if (!validationResult.success) {
-    return res.status.json({ error: validationResult.error });
+    return res.status(400).json({ error: validationResult.error });
   }
 
   try {
@@ -105,13 +105,22 @@ const updateWarehouse = async (req, res) => {
 
   const { created_at, updated_at, ...updatedData } = req.body;
 
+  const currentDateTime = new Date()
+    .toISOString()
+    .slice(0, 19)
+    .replace("T", " ");
+
+  updatedData.updated_at = currentDateTime;
+
   const sql = `UPDATE warehouses SET ? WHERE warehouses.id = ?`;
 
   try {
     const [results] = await connection.query(sql, [updatedData, warehouseId]);
 
     if (results.affectedRows === 0) {
-      res.status(404).json({ msg: `No record with ID${warehouseId} found` });
+      return res
+        .status(404)
+        .json({ msg: `No record with ID ${warehouseId} found` });
     }
 
     res.json({ message: `Warehouse ${warehouseId} has been updated` });
